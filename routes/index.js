a2c51ed3f8410756d6172bd3e878b9ad5c0aaa47
FernandoJosueCarbajal <fernandocarbajal2010@hotmail.com>
2017-06-06 19:23:01 -0600

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/examen', function(req,res,next){
+  var examen = [
+    {
+      "Nombre":"Fernando Josue Carbajal pavon",
+      "Identidad": "0801199110451",
+      "Correo": "fernandocarbajal2010@hotmail.com",
+    }];
+  res.json(examen);
+
+router.post('/suma',function(req,res,next){
+    var entero1 = 2
+    var entero2 = 3
+    var suma = entero1 +entero2
+    res.render('mensaje',suma);
+    });
+
 module.exports = router;
